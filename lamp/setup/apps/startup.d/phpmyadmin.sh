54ff9774a7c5e7e6b6e94643c1eec5cb2d8187a5
Gary Wisniewski <garyw@blueseastech.com.au>
2015-07-19 13:46:25 +1000

    - Removed symlinks to phpmyadmin.  Makes derivative Dockerfiles easier. - Use SIGQUIT for phpfpm service

--- lamp/setup/apps/startup.d/phpmyadmin.sh
+++ lamp/setup/apps/startup.d/phpmyadmin.sh
@@ -9,9 +9,3 @@ if [ $CONTAINER_INIT == 1 ]; then
   sudo bash -c "chown -R $puser: /var/lib/phpmyadmin/tmp; chgrp --reference /var/lib/phpmyadmin/tmp /var/lib/phpmyadmin/*.php"
   sudo bash -c "chgrp --reference /var/lib/phpmyadmin/tmp \`find /etc/phpmyadmin -group www-data\`"
 fi
-
-if [ $APPS_INIT == 1 ]; then
-  dolog creating phpmyadmin link in default site
-  cd $APPS_DIR/www/default
-  ln -s /usr/share/phpmyadmin
-fi
